860077e41413fe156368cccba5018e50a679b027
Erkan Demir <122565958+erkanndemirr@users.noreply.github.com>
2024-11-25 01:32:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const nextConfig = {
   webpack(config) {
     // '@public' alias'ını 'public' dizinine yönlendirmek için
     config.resolve.alias['@public'] = path.resolve('public');
-        if (!isServer) {
-      config.resolve.fallback = {
-        canvas: false, // canvas modülünü tarayıcı tarafında devre dışı bırak
-      };
     }
     return config;
   }
